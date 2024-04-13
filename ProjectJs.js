3a83e65ffb11966778ae93a7b5f7d29db629113f
farisIH <165578801+farisIH@users.noreply.github.com>
2024-04-13 20:14:12 +0700

    Update ProjectJs.js

---
+++ ProjectJs.js
@@ -0,0 +1,39 @@
+		let slideIndex = 1;
+		showSlides(slideIndex);
+		
+		function plusSlides(n) {
+		  showSlides(slideIndex += n);
+		}
+		
+		function currentSlide(n) {
+		  showSlides(slideIndex = n);
+		}
+		
+		function showSlides(n) {
+		  let i;
+		  let slides = document.getElementsByClassName("mySlides");
+		  let dots = document.getElementsByClassName("dot");
+		  if (n > slides.length) {slideIndex = 1}    
+		  if (n < 1) {slideIndex = slides.length}
+		  for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";  
+		  }
+		  for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		  }
+		  slides[slideIndex-1].style.display = "block";  
+		  dots[slideIndex-1].className += " active";
+		}
+
+		// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+		window.onscroll = function() {scrollFunction()};
+
+		function scrollFunction() {
+		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+			document.getElementById("navbar").style.padding = "30px 10px";
+			document.getElementById("logo").style.fontSize = "25px";
+		} else {
+			document.getElementById("navbar").style.padding = "80px 10px";
+			document.getElementById("logo").style.fontSize = "35px";
+		}
+		}
